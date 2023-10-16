0b9cd736651d300a06591d20c61de0e2832afa99
Alberto clase <DAM2A-03@JOVELLANOS.EDU>
2023-10-16 10:49:48 +0200

    fin intentos y radiobuttons

--- AppAdivina/app/src/main/java/colmenar/alberto/appadivina/MainActivity.java
+++ AppAdivina/app/src/main/java/colmenar/alberto/appadivina/MainActivity.java
@@ -7,47 +7,92 @@ import android.view.View;
 import android.view.KeyEvent;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int numeroOculto = 37;
+    private int numeroOculto;
     private int numeroJugado;
+    private int numIntentos;
     EditText et = null;
     TextView textoIzq = null;
+    TextView textoIntentos = null;
+    Button botonProbar = null;
+    Button botonVolverAJugar = null;
+    TextView textoVolverAJugar = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //elegir numero
-        //Random dado = new Random();
-        //int numeroOculto = dado.nextInt(100) + 1;
         et = (EditText) findViewById(R.id.cajaTexto);
         textoIzq = (TextView) findViewById(R.id.textoIzq);
+        textoIntentos = (TextView) findViewById(R.id.textoIntentos);
+        botonProbar = (Button) findViewById(R.id.botonProbar);
+        botonVolverAJugar = (Button) findViewById(R.id.botonVolverAJugar);
+        botonVolverAJugar.setVisibility(View.INVISIBLE);
+        textoVolverAJugar = (TextView) findViewById(R.id.textoVolverAJugar);
+        textoVolverAJugar.setVisibility(View.INVISIBLE);
+
+        crearNumeroAleatorio();
         et.setOnKeyListener(new View.OnKeyListener() {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if ((event.getAction() == keyEvent.ACTION_DOWN) && (keyCode == keyEvent.KEYCODE_ENTER)) {
+                if ((event.getAction() == android.view.KeyEvent.ACTION_DOWN) && (keyCode == android.view.KeyEvent.KEYCODE_ENTER)) {
                     probar(v);
                     return true;
                 } else return false;
             }
         });
     }
-
+    private void crearNumeroAleatorio() {
+        numIntentos = 0;
+        Random dado = new Random();
+        numeroOculto = dado.nextInt(100) + 1;
+    }
     public void probar(View v) {
         String mensaje;
         String mensajeFinal;
+        System.out.println(numeroOculto);
         numeroJugado = Integer.parseInt(et.getText().toString());
-        if (numeroJugado < numeroOculto) {
+        if (numeroJugado > numeroOculto) {
             mensaje = getResources().getString(R.string.textoMenor);
             mensajeFinal = String.format(mensaje, numeroJugado);
-        } else if (numeroJugado > numeroOculto) {
+        } else if (numeroJugado < numeroOculto) {
             mensaje = getResources().getString(R.string.textoMayor);
             mensajeFinal = String.format(mensaje, numeroJugado);
         } else {
+            finalPartida();
             mensajeFinal = getResources().getString(R.string.textoAcierto);
         }
         textoIzq.setText(mensajeFinal);
         et.setText("");
+        actualizarIntentos();
+    }
+    private void actualizarIntentos() {
+        numIntentos++;
+        String texto = getResources().getQuantityString(R.plurals.intentos, numIntentos, numIntentos);
+        textoIntentos.setText(texto);
+    }
+    private void finalPartida() {
+        botonVolverAJugar.setVisibility(View.VISIBLE);
+        textoVolverAJugar.setVisibility(View.VISIBLE);
+        et.setVisibility(View.INVISIBLE);
+        botonProbar.setVisibility(View.INVISIBLE);
+        textoIzq.setVisibility(View.INVISIBLE);
+        textoIntentos.setVisibility(View.INVISIBLE);
+    }
+    public void jugar(View v) {
+        crearNumeroAleatorio();
+        botonVolverAJugar.setVisibility(View.INVISIBLE);
+        textoVolverAJugar.setVisibility(View.INVISIBLE);
+        et.setVisibility(View.VISIBLE);
+        botonProbar.setVisibility(View.VISIBLE);
+        textoIzq.setVisibility(View.VISIBLE);
+        textoIntentos.setVisibility(View.VISIBLE);
+        textoIzq.setText(R.string.textoIzq);
+        textoIntentos.setText("");
     }
 }
--- AppRadioGroup/app/src/main/java/colmenar/alberto/appradiogroup/MainActivity.java
+++ AppRadioGroup/app/src/main/java/colmenar/alberto/appradiogroup/MainActivity.java
@@ -4,23 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    RadioGroup opciones;
+    TextView textoRespuesta;
+    CheckBox chCamara;
+    CheckBox chBluetooth;
+    CheckBox chSonido;
+    TextView textoCheckbox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        opciones = (RadioGroup) findViewById(R.id.radioGroup);
+        opciones = (RadioGroup) findViewById(R.id.radioGroup);
+        textoRespuesta = (TextView) findViewById(R.id.textoRespuesta);
+        chCamara = (CheckBox) findViewById(R.id.checkBoxCamara);
+        chBluetooth = (CheckBox) findViewById(R.id.checkBoxBluetooth);
+        chSonido = (CheckBox) findViewById(R.id.checkBoxSonido);
+        textoCheckbox = (TextView) findViewById(R.id.textoCheckbox);
 
+        opciones.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                comprobarRadios();
+            }
+        });
     }
 
     public void botonAceptar(View v) {
-        RadioGroup opciones = (RadioGroup) findViewById(R.id.radioGroup);
-        TextView textoRespuesta = (TextView) findViewById(R.id.textoRespuesta);
-        System.out.println(R.id.radio1);
-        System.out.println(opciones.getCheckedRadioButtonId());
+        comprobarRadios();
+        if (chCamara.isChecked()) {
+            textoCheckbox.append("\n" + getString(R.string.textoCamara));
+        }
+        if (chBluetooth.isChecked()) {
+            textoCheckbox.append("\n" + getString(R.string.textoBluetooth));
+        }
+        if (chSonido.isChecked()) {
+            textoCheckbox.append("\n" + getString(R.string.textoSonido));
+        }
+    }
+    public void comprobarRadios() {
         int opcionDada = opciones.getCheckedRadioButtonId();
         if (opcionDada == R.id.radio1) {
             textoRespuesta.setText(R.string.respAndroid);
