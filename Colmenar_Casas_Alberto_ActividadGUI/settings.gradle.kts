53a1831cc226217af986f4459707b207ea4f47f8
Alberto clase <DAM2A-03@JOVELLANOS.EDU>
2023-10-17 11:26:23 +0200

    diseño

---
+++ Colmenar_Casas_Alberto_ActividadGUI/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Colmenar_Casas_Alberto_ActividadGUI"
+include(":app")
+ 
