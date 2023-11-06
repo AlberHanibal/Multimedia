d58941e0dca1202d9aa5b34b669c848fb1b1c4d5
Alberto clase <DAM2A-03@JOVELLANOS.EDU>
2023-11-06 10:29:16 +0100

    fin clase

---
+++ AppIntent/app/src/main/java/colmenar/casas/alberto/appintent/MainActivity.java
@@ -0,0 +1,21 @@
+package colmenar.casas.alberto.appintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onPulsame(View v) {
+        Intent i = new Intent (this, SegundaActividad.class);
+        startActivity(i);
+    }
+}
